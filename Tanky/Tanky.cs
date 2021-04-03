efc7dfd33c474130191655fa7f3a571dc5182638
José Manuel Nieto Sánchez <superjmn@outlook.com>
2021-04-04 00:10:48 +0200

    Add project files.

---
+++ Tanky/Tanky.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tanky
+{
+    class Tanky
+    {
+        public Tanky(Texture2D texture)
+        {
+            Texture = texture;
+        }
+
+        public Texture2D Texture { get; }
+        public Vector2 Position { get; set; }
+    }
+}
